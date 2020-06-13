4a270eebeda865f03245b6673bf5b60cabb4f424
weather-application <66865031+weather-application@users.noreply.github.com>
2020-06-13 22:23:31 +0700

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -58,39 +58,12 @@ document.getElementById('searchBtn').addEventListener('click', () => {
         searchWeather(searchTerm);
 });
 
-navigator.serviceWorker.register('/sw.js', {scope: "/"})
-    .then(
-    function (reg) {
-        var serviceWorker;
-        if (reg.installing) {
-            serviceWorker = reg.installing;
-            // console.log('Service worker installing');
-        } else if (reg.waiting) {
-            serviceWorker = reg.waiting;
-            // console.log('Service worker installed & waiting');
-        } else if (reg.active) {
-            serviceWorker = reg.active;
-            // console.log('Service worker active');
-        }
 
-        if (serviceWorker) {
-            console.log("sw current state", serviceWorker.state);
-            if (serviceWorker.state == "activated") {
-                //If push subscription wasnt done yet have to do here
-                console.log("sw already activated - Do watever needed here");
-            }
-            serviceWorker.addEventListener("statechange", function(e) {
-                console.log("sw statechange : ", e.target.state);
-                if (e.target.state == "activated") {
-                    // use pushManger for subscribing here.
-                    console.log("Just now activated. now we can subscribe for push notification")
-                    subscribeForPushNotification(reg);
-                }
-            });
-        }
-    },
-    function (err) {
-        console.error('unsuccessful registration with ', '/sw.js', err);
-    }
-);
-
+navigator.serviceWorker.register('/sw.js');
+var deferredPrompt;
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
