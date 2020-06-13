62237187d1c9c54e6d7943bd556f580ec47058b6
weather-application <66865031+weather-application@users.noreply.github.com>
2020-06-13 19:14:24 +0700

    Add files via upload

---
+++ src/js/app.js
@@ -0,0 +1,69 @@
+let appId = 'afb4765b403008cec2c721a6e50347a4';
+let units = 'metric'; 
+let searchMethod; // q means searching as a string.
+
+function getSearchMethod(searchTerm) {
+    if(searchTerm.length === 5 && Number.parseInt(searchTerm) + '' === searchTerm)
+        searchMethod = 'zip';
+    else 
+        searchMethod = 'q';
+}
+
+function searchWeather(searchTerm) {
+    getSearchMethod(searchTerm);
+    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`)
+        .then((result) => {
+            return result.json();
+        }).then((res) => {
+            init(res);
+    });
+   
+}
+
+function init(resultFromServer) {
+    switch (resultFromServer.weather[0].main) {
+        
+    }
+
+    let weatherDescriptionHeader = document.getElementById('weatherDescriptionHeader');
+    let temperature = document.getElementById('temperature');
+    let humidity = document.getElementById('humidity');
+    let windSpeed = document.getElementById('windSpeed');
+    let cityHeader = document.getElementById('cityHeader');
+    let tempMin = document.getElementById('tempMin');
+    let tempMax = document.getElementById('tempMax');
+    let feelsLike = document.getElementById('feelsLike');
+    let weatherIcon = document.getElementById('documentIconImg');
+    weatherIcon.src = 'http://openweathermap.org/img/w/' + resultFromServer.weather[0].icon + '.png';
+
+    let resultDescription = resultFromServer.weather[0].description;
+    weatherDescriptionHeader.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
+    temperature.innerHTML = Math.floor(resultFromServer.main.temp) + '&#176;';
+    windSpeed.innerHTML = 'Wind Speed: ' + Math.floor(resultFromServer.wind.speed) + ' meter/s';
+    cityHeader.innerHTML = resultFromServer.name;
+    humidity.innerHTML = 'Humidity levels: ' + resultFromServer.main.humidity +  '%';
+    tempMin.innerHTML = 'Temp Min:' + Math.floor(resultFromServer.main.temp_min) + '&#176;';
+    tempMax.innerHTML = 'Temp Max:' + Math.floor(resultFromServer.main.temp_max) + '&#176;';
+    feelsLike.innerHTML = 'Feels Like:' + Math.floor(resultFromServer.main.feels_like) + '&#176;' ;
+    console.log(resultFromServer);
+
+    
+}
+
+
+
+document.getElementById('searchBtn').addEventListener('click', () => {
+    let searchTerm = document.getElementById('searchInput').value;
+    if(searchTerm)
+        searchWeather(searchTerm);
+});
+
+
+navigator.serviceWorker.register('/sw.js');
+var deferredPrompt;
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
