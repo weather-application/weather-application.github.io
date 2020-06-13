8cb54c60bb6cbf403c79adca7b57d5b063f2487a
weather-application <66865031+weather-application@users.noreply.github.com>
2020-06-13 21:51:10 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,7 +6,10 @@ self.addEventListener('install', function(event) {
             '/',
             '/index.html',
             '/src/css/app.css',
-            '/src/js/app.js'
+            '/src/js/app.js',
+            'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css',
+            'https://use.fontawesome.com/releases/v5.7.2/css/all.css',
+            'https://fonts.googleapis.com/css?family=Bungee|Hind|PT+Sans|Pacifico|Poppins|Lobster'
           ])
         })
     );
