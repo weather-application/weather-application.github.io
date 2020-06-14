1b6c11855721484e169ddcc40defc6c423e13588
weather-application <66865031+weather-application@users.noreply.github.com>
2020-06-14 15:58:20 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,4 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('first-app')
-        .then(function(cache) {
-          cache.addAll([
+const FILES_TO_CACHE = [
             '/',
             '/index.html',
             '/src/css/app.css',
@@ -12,18 +8,10 @@ self.addEventListener('install', function(event) {
             'https://code.jquery.com/jquery-3.5.1.slim.min.js',
             'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js',
             'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js'
-          ])
-        })
-    );
-    return self.clients.claim();
-  });
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request)
-        .then(function(res) {
-          return res;
-        })
-    );
-  });
-
-
+];
+evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Pre-caching offline page');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+);
