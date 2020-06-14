7dd974db13e39bdeff218ed50d9028b48530152d
weather-application <66865031+weather-application@users.noreply.github.com>
2020-06-14 20:22:01 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,6 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('first-app')
-        .then(function(cache) {
-          cache.addAll([
-            '/',
-            '/index.html',
+const FILES_TO_CACHE = [
+'/',
+ '/index.html',
             '/src/css/app.css',
             '/src/js/app.js',
             'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css',
@@ -12,36 +8,10 @@ self.addEventListener('install', function(event) {
             'https://code.jquery.com/jquery-3.5.1.slim.min.js',
             'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js',
             'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js'
-          ])
-        })
-    );
-    return self.clients.claim();
-  });
-'use strict';
-
-// CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v1';
-
-// CODELAB: Add list of files to cache here.
-const FILES_TO_CACHE = [
 ];
-
-self.addEventListener('install', (evt) => {
-  console.log('[ServiceWorker] Install');
-  // CODELAB: Precache static resources here.
-
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (evt) => {
-  console.log('[ServiceWorker] Activate');
-  // CODELAB: Remove previous cached data from disk.
-
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (evt) => {
-  console.log('[ServiceWorker] Fetch', evt.request.url);
-  // CODELAB: Add fetch event handler here.
-
-});
+evt.waitUntil(
+caches.open(CACHE_NAME).then((cache) => {
+console.log('[ServiceWorker] Pre-caching offline page');
+return cache.addAll(FILES_TO_CACHE);
+})
+);
