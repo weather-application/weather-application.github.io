58607ee0de0d19c7557bf06038110c1701742cec
weather-application <66865031+weather-application@users.noreply.github.com>
2020-06-14 20:32:51 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,15 @@
+
+var CACHE_STATIC_NAME = 'static-v4';
+var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
-  self.skipWaiting(); //PENTING bila ada versi baru!!
   event.waitUntil(
-    caches.open('static')
+    caches.open(CACHE_STATIC_NAME)
       .then(function(cache) {
         console.log('[Service Worker] Precaching App Shell');
         cache.addAll([
-            '/',
+          '/',
             '/index.html',
             '/src/css/app.css',
             '/src/js/app.js',
@@ -14,8 +17,47 @@ self.addEventListener('install', function(event) {
             'https://use.fontawesome.com/releases/v5.7.2/css/all.css',
             'https://code.jquery.com/jquery-3.5.1.slim.min.js',
             'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js',
-            'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js
+            'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js'
         ]);
       })
   )
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activating Service Worker ....', event);
+  event.waitUntil(
+    caches.keys()
+      .then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log('[Service Worker] Removing old cache.', key);
+            return caches.delete(key);
+          }
+        }));
+      })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        } else {
+          return fetch(event.request)
+            .then(function(res) {
+              return caches.open(CACHE_DYNAMIC_NAME)
+                .then(function(cache) {
+                  cache.put(event.request.url, res.clone());
+                  return res;
+                })
+            })
+            .catch(function(err) {
+
+            });
+        }
+      })
+  );
 });
