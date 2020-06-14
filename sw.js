5f4e5cc5d5bab6e8f06418391fbb5758c7b9760e
weather-application <66865031+weather-application@users.noreply.github.com>
2020-06-14 13:13:38 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -25,4 +25,19 @@ self.addEventListener('install', function(event) {
         })
     );
   });
-  
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request)
+      .then(function(res) {
+        return caches.open(CACHE_DYNAMIC_NAME)
+                .then(function(cache) {
+                  cache.put(event.request.url, res.clone());
+                  return res;
+                })
+      })
+      .catch(function(err) {
+        return caches.match(event.request);
+      })
+  );
+});
+
