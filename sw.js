0687449784fbb3a6f657ca6197f0ea63b37ef365
weather-application <66865031+weather-application@users.noreply.github.com>
2020-06-14 20:48:50 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,12 @@
+
 var CACHE_STATIC_NAME = 'static-v1';
 var CACHE_DYNAMIC_NAME = 'dynamic-v2';
 
-self.addEventListener('install', function (event) {
+self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
   event.waitUntil(
     caches.open(CACHE_STATIC_NAME)
-      .then(function (cache) {
+      .then(function(cache) {
         console.log('[Service Worker] Precaching App Shell');
         cache.addAll([
           '/',
@@ -22,12 +23,12 @@ self.addEventListener('install', function (event) {
   )
 });
 
-self.addEventListener('activate', function (event) {
+self.addEventListener('activate', function(event) {
   console.log('[Service Worker] Activating Service Worker ....', event);
   event.waitUntil(
     caches.keys()
-      .then(function (keyList) {
-        return Promise.all(keyList.map(function (key) {
+      .then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
           if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
             console.log('[Service Worker] Removing old cache.', key);
             return caches.delete(key);
@@ -37,58 +38,32 @@ self.addEventListener('activate', function (event) {
   );
   return self.clients.claim();
 });
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.match(event.request)
-//       .then(function(response) {
-//         if (response) {
-//           return response;
-//         } else {
-//           return fetch(event.request)
-//             .then(function(res) {
-//               return caches.open(CACHE_DYNAMIC_NAME)
-//                 .then(function(cache) {
-//                   cache.put(event.request.url, res.clone());
-//                   return res;
-//                 })
-//             })
-//             .catch(function(err) {
-//               return caches.open(CACHE_STATIC_NAME)
-//                 .then(function(cache) {
-//                   return cache.match('/offline.html');
-//                 });
-//             });
-//         }
-//       })
-//   );
-// });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    fetch(event.request)
-      .then(function(res) {
-        return caches.open(CACHE_DYNAMIC_NAME)
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        } else {
+          return fetch(event.request)
+            .then(function(res) {
+              return caches.open(CACHE_DYNAMIC_NAME)
                 .then(function(cache) {
                   cache.put(event.request.url, res.clone());
                   return res;
                 })
-      })
-      .catch(function(err) {
-        return caches.match(event.request);
+            })
+            .catch(function(err) {
+
+            });
+        }
       })
   );
 });
-
-// Cache-only
-// self.addEventListener('fetch', function (event) {
-//   event.respondWith(
-//     caches.match(event.request)
-//   );
-// });
-
-// Network-only
-// self.addEventListener('fetch', function (event) {
-//   event.respondWith(
-//     fetch(event.request)
-//   );
-// });
+//Cache-only
+ self.addEventListener('fetch', function (event) {
+   event.respondWith(
+    caches.match(event.request)
+   );
+ })
